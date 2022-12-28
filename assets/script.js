32ce362c1696cc24232ba345a4344b92aabe8419
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2022-12-28 11:35:02 -0800

    removed console.logs()

--- assets/script.js
+++ assets/script.js
@@ -21,33 +21,23 @@ searchButton.on('click', function (event) {
     
     getCity(cityName);  
     $("#cityName").val("");
-    // console.log(history);
-    // console.log("cityName: ", cityName);
-    // console.log(history);
     });
 
 //--------calls to weather API------>
 function getCity(cityName) {
     //----- geocoding for lat and lon of city----->
     let geoCityApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=25c2936adbc148c96a60f0af93156bc8";
-    console.log(geoCityApi);
     fetch(geoCityApi)
         .then(function (response) {
-        // console.log(response);
         return response.json();
         
         })
         .then(function (data) {
-
             const lat = data[0].lat;
-            // console.log(lat);
             const lon = data[0].lon;
-            // console.log(lon);
             var latlon = lat.toString() + ", " + lon.toString();
             window.localStorage.setItem(cityName, latlon);
 
-            // console.log(data);
-            // console.log(latlon);
             weatherNow (lat, lon);
         });
 };
@@ -55,8 +45,6 @@ function getCity(cityName) {
 //using the lat and lon of city to obtain weather data
 function weatherNow(lat, lon) {
     let apiURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=25c2936adbc148c96a60f0af93156bc8";
-    console.log(apiURL);
-
 
     $(".results-panel").addClass("visible");
     //api to get current weather data
@@ -67,15 +55,10 @@ function weatherNow(lat, lon) {
         .then(function(data) {
             let name = data.name;
             let iconcode = data.weather[0].icon;
-            
-            
-            console.log(iconcode);
 
             $(".todayCity").text(name);
             $(".todayDate").text(date);
-
             $("#wicon").attr("src", iconUrl + iconcode + ".png"); 
-
             var tempValue = data.main.temp;
             $(".temp").text("Temperature: " + tempValue + "°F");
             var condition = data.weather[0].main;
@@ -87,7 +70,6 @@ function weatherNow(lat, lon) {
     })
 
     const forcastApi = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=25c2936adbc148c96a60f0af93156bc8";
-    console.log(forcastApi);
     //api to get future weather data
     fetch(forcastApi)
         .then(function(response) {
@@ -104,19 +86,15 @@ function weatherNow(lat, lon) {
                 
                 const iconcode = data.list[index].weather[0].icon;
                 $("#day" + num + "icon").attr("src", iconUrl + iconcode + ".png");
-                // console.log(iconcode);
-                // console.log(iconUrl)
 
                 const temp = data.list[index].main.temp;
                 $("#day" + num + "temp").text("Temperature: " + temp + "°F");
-                // console.log(temp);
 
                 const humid = data.list[index].main.humidity;
                 $("#day" + num + "humidity").text("Humidity: " + humid + "%");
 
                 const wind = data.list[index].wind.speed;
                 $("#day" + num + "wind").text("Wind Speeds: " + wind + "mph");
-                // console.log(wind);
 
             num++;
                 }}
@@ -132,7 +110,6 @@ function cityHistory() {
         $(`#${history[index]}`).on("click", function (event) {
         event.preventDefault();
         let cityName = $(this).text();
-        console.log("clicked " + this.textContent);
         getCity(cityName);
         }
       );
